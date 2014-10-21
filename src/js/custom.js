22a409a5f42c9ebec385aec36908f4743e66c21b
Keav <chris@keav.net>
2014-10-21 15:46:47 +0100

    Minor tweaks to custom.js
    Load jQuery from Google CDN (with local fallback)

--- src/js/custom.js
+++ src/js/custom.js
@@ -4,24 +4,27 @@
 
 "use strict";
 
-// Uses Modernizr.js to detect touch enabled devices. This method is an indelicate solution but the best I've found so far. Agent sniffing or actual property testing resulted in inconsisstent results.
-//It then disables the CSS 'background-attachment:fixed;' property. This because fixed backgrounds don't play nicely on iOS devices when scrolling. iOS freezes all animations/transitons until the scroll is finished rather than animating them as the page scrolls.
-if ($("html").hasClass("no-touch")) {
-    $('.splash-background, .divider-image-cover, html, body').addClass('fix-background');
-} else {
-    $('.splash-background, .divider-image-cover, html, body').removeClass('fix-background');
-}
 
-$('#js-anim').hover(function () {
-    $(this).removeClass('bounce');
-}, function () {
-    $(this).addClass('bounce');
+$(document).ready(function () {
+    // Uses Modernizr.js to detect touch enabled devices. This method is an indelicate solution but the best I've found so far. Agent sniffing or actual property testing resulted in inconsisstent results.
+    //It then disables the CSS 'background-attachment:fixed;' property. This because fixed backgrounds don't play nicely on iOS devices when scrolling. iOS freezes all animations/transitons until the scroll is finished rather than animating them as the page scrolls.
+    if ($("html").hasClass("no-touch")) {
+        $('.splash-background, .divider-image-cover, html, body').addClass('fix-background');
+    } else {
+        $('.splash-background, .divider-image-cover, html, body').removeClass('fix-background');
+    }
 });
 
 $(document).ready(function () {
+    $('#js-anim').hover(function () {
+        $(this).removeClass('bounce');
+    }, function () {
+        $(this).addClass('bounce');
+    });
+});
 
+$(document).ready(function () {
     var owl = $("#owl-portfolio");
-
     owl.owlCarousel({
         autoplay: false,
         singleItem: true,
@@ -38,6 +41,7 @@ $(document).ready(function () {
     });
 });
 
+
 var waitForFinalEvent = (function () {
     var timers = {};
     return function (callback, ms, uniqueId) {
@@ -50,6 +54,7 @@ var waitForFinalEvent = (function () {
         timers[uniqueId] = setTimeout(callback, ms);
     };
 }());
+
 
 $(document).ready(function () {
     var i = 0, imgHeight = $("#getHeight").height(), myElements = document.querySelectorAll(".owl-prev, .owl-next");
@@ -93,7 +98,7 @@ $(window).trigger('resize');
 /*jslint unparam:true */
 $(document).ready(function () {
     $("body").click(function (event) {
-        // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called 
+        // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called
         if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible")) {
             $('.navbar-collapse').collapse('toggle');
             $('body').removeClass('pointer');
@@ -120,15 +125,15 @@ function preventDefault(e) {
     }
 }
 
-function keydown(e) {
-    var i = 0;
-    for (i = keys.length; i--;) {
-        if (e.keyCode === keys[i]) {
-            preventDefault(e);
-            return;
-        }
-    }
-}
+// function keydown(e) {
+//     var i = 0;
+//     for (i = keys.length; i--;) {
+//         if (e.keyCode === keys[i]) {
+//             preventDefault(e);
+//             return;
+//         }
+//     }
+// }
 
 function wheel(e) {
     preventDefault(e);
@@ -150,85 +155,93 @@ function enable_scroll() {
     window.onmousewheel = document.onmousewheel = document.onkeydown = null;
 }
 
+
+$(document).ready(function () {
 // Navbar fading
 //Uses waypoints.js to trigger an action. In this case enabling mouse scrolling using enable_scroll(); function and adding/removing CSS classes.
-var z = 0;
-$('#js-scroll-trigger').waypoint(function (direction) {
-    var el = '.navbar';
-    if (direction === 'down') {
+    var z = 0;
+    $('#js-scroll-trigger').waypoint(function (direction) {
+        var el = '.navbar', tEnd = 'transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd';
+        if (direction === 'down') {
 
-        $(el).removeClass('fade-out navbar-hide');
-        $(el).addClass('fade-in');
-        z = 0;
+            $(el).removeClass('fade-out navbar-hide');
+            $(el).addClass('fade-in');
+            z = 0;
 
-    } else {
+        } else {
 
-        $(el).removeClass('fade-in');
-        $(el).addClass('fade-out');
-        z = 1;
+            $(el).removeClass('fade-in');
+            $(el).addClass('fade-out');
+            z = 1;
 
-        $(el).on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
-            if (z === 1) {
-                $(el).addClass('navbar-hide');
-                z = 0;
-                $(el).off('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd');
-            }
-        });
-    }
-}, {
-    offset: '0%'
-});
-
-
-//Uses waypoints.js to trigger an action. In this case disabling mouse scrolling using disable_scroll(); function and scrollTop to #ID.
-//Is triggered by hrefs whether mouse is scrolled or not!!! i.e. is triggered by SCREEN movement.
-var x = 0;
-$('#js-scroll-trigger').waypoint(function (direction) {
-    if (!$('html, body').is(':animated') && (direction === 'down') && x === 0) {
-        disable_scroll();
-        x = 1;
-
-        $('html, body').stop().animate({
-            scrollTop: $('#anchor-portfolio').offset().top
-        }, 1000, 'easeOutQuad');
-    }
-
-    if (x === 1) {
-        // Using 'body' instead of '.navbar' fires for all occuring transitions i.e. fires multiple times.
-        // Perhaps detect position of page rather than transitions?
-        $('.navbar').on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
-            enable_scroll();
-            x = 2;
-            $('.navbar').off('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd');
-        });
-    }
-}, {offset: '100%'});
-
-
-//Easing href link functions using jquery.easing.1.3.js
-//Vertical easing ".js-ver"
-$(function () {
-    $('.js-ver a').bind('click', function (event) {
-        var $anchor = $(this);
-
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-
-        event.preventDefault();
+            $(el).on(tEnd, function () {
+                if (z === 1) {
+                    $(el).addClass('navbar-hide');
+                    z = 0;
+                    $(el).off(tEnd);
+                }
+            });
+        }
+    }, {
+        offset: '0%'
     });
 });
 
-//Horizontal easing ".js-hor"
-$(function () {
-    $('.js-hor a').bind('click', function (event) {
-        var $anchor = $(this);
+$(document).ready(function () {
+//Uses waypoints.js to trigger an action. In this case disabling mouse scrolling using disable_scroll(); function and scrollTop to #ID.
+//Is triggered by hrefs whether mouse is scrolled or not!!! i.e. is triggered by SCREEN movement.
+    var x = 0, tEnd = 'transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd';
+    $('#js-scroll-trigger').waypoint(function (direction) {
+        if (!$('html, body').is(':animated') && (direction === 'down') && x === 0) {
+            disable_scroll();
+            x = 1;
 
-        $('html, body').stop().animate({
-            scrollLeft: $($anchor.attr('href')).offset().left
-        }, 1500, 'easeInOutExpo');
+            $('html, body').stop().animate({
+                scrollTop: $('#anchor-portfolio').offset().top
+            }, 1000, 'easeOutQuad');
+        }
 
-        event.preventDefault();
+        if (x === 1) {
+            // Using 'body' instead of '.navbar' fires for all occuring transitions i.e. fires multiple times.
+            // Perhaps detect position of page rather than transitions?
+            $('.navbar').on(tEnd, function () {
+                enable_scroll();
+                x = 2;
+                $('.navbar').off(tEnd);
+            });
+        }
+    }, {offset: '100%'});
+});
+
+
+$(document).ready(function () {
+    //Easing href link functions using jquery.easing.1.3.js
+    //Vertical easing ".js-ver"
+    $(function () {
+        $('.js-ver a').bind('click', function (event) {
+            var $anchor = $(this);
+
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+
+            event.preventDefault();
+        });
+    });
+});
+
+$(document).ready(function () {
+    //Horizontal easing ".js-hor"
+    $(function () {
+        $('.js-hor a').bind('click', function (event) {
+            var $anchor = $(this);
+
+            $('html, body').stop().animate({
+                scrollLeft: $($anchor.attr('href')).offset().left
+            }, 1500, 'easeInOutExpo');
+
+            event.preventDefault();
+        });
     });
 });
 
